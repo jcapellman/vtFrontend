d30731ee2833292135505809da0618b858eb3304
Jarred Capellman <jcapellman@hotmail.com>
2020-12-26 15:43:56 -0600

    Added: BaseVewModel class for the MVVM

---
+++ vtFrontend.GUI/ViewModels/Base/BaseVewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using vtFrontend.GUI.Annotations;
+
+namespace vtFrontend.GUI.ViewModels.Base
+{
+    public class BaseVewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
