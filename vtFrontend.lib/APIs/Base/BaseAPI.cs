159bd32c9479b23dafb54ae5cd753d32d6651bdc
Jarred Capellman <jcapellman@hotmail.com>
2020-12-26 15:53:19 -0600

    Added: GetByteAsync base method to the BaseAPI class

--- vtFrontend.lib/APIs/Base/BaseAPI.cs
+++ vtFrontend.lib/APIs/Base/BaseAPI.cs
@@ -1,16 +1,31 @@
-﻿using vtFrontend.lib.Objects;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+using vtFrontend.lib.Objects;
 
 namespace vtFrontend.lib.APIs.Base
 {
     public abstract class BaseAPI
     {
-        private SettingsItem _settings;
+        private const string VtBaseurl = "https://www.virustotal.com/api/v3/";
+        
+        private readonly SettingsItem _settings;
 
         protected BaseAPI(SettingsItem settings)
         {
             _settings = settings;
         }
 
-        public abstract bool Run(string[] parameters);
+        protected async Task<byte[]> GetByteAsync(string url)
+        {
+            using var httpClient = new HttpClient();
+            
+            httpClient.DefaultRequestHeaders.Add("x-apikey", _settings.VTKey);
+
+            return await httpClient.GetByteArrayAsync(
+                $"{VtBaseurl}{url}");
+        }
+        
+        public abstract Task<bool> RunAsync(string[] parameters);
     }
 }
