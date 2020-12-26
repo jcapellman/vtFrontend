5814042da2355d85173ef9f6711a4c83445b8134
Jarred Capellman <jcapellman@hotmail.com>
2020-12-26 15:44:28 -0600

    Added: Initial work on the BaseAPI class

---
+++ vtFrontend.lib/APIs/Base/BaseAPI.cs
@@ -0,0 +1,16 @@
+﻿using vtFrontend.lib.Objects;
+
+namespace vtFrontend.lib.APIs.Base
+{
+    public abstract class BaseAPI
+    {
+        private SettingsItem _settings;
+
+        protected BaseAPI(SettingsItem settings)
+        {
+            _settings = settings;
+        }
+
+        public abstract bool Run(string[] parameters);
+    }
+}
