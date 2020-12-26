f562e4b85f9b5ed464308576658650ce5eb8c5f4
Jarred Capellman <jcapellman@hotmail.com>
2020-12-26 15:44:46 -0600

    Added: Initial work on the MainViewModel class

---
+++ vtFrontend.GUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using vtFrontend.GUI.ViewModels.Base;
+
+namespace vtFrontend.GUI.ViewModels
+{
+    public class MainViewModel : BaseVewModel
+    {
+        private string _vtKey;
+
+        public string VTKey
+        {
+            get => _vtKey;
+
+            set
+            {
+                _vtKey = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
