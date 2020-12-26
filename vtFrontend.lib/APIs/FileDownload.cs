113e28e2a4eeb52f021d314812a1e88b35a72e48
Jarred Capellman <jcapellman@hotmail.com>
2020-12-26 15:45:29 -0600

    Added: Initial work on the FileDownload class

---
+++ vtFrontend.lib/APIs/FileDownload.cs
@@ -0,0 +1,17 @@
+﻿using vtFrontend.lib.APIs.Base;
+using vtFrontend.lib.Objects;
+
+namespace vtFrontend.lib.APIs
+{
+    public class FileDownload : BaseAPI
+    {
+        public FileDownload(SettingsItem settings) : base(settings)
+        {
+        }
+        
+        public override bool Run(string[] parameters)
+        {
+            
+        }
+    }
+}
