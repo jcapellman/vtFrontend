33bd1541bd88e408f12ddd6fee3e7e1d265a62a4
Jarred Capellman <jcapellman@hotmail.com>
2020-12-27 15:08:34 -0600

    Added: FileHash implementation of the BaseParameter class

---
+++ vtFrontend.lib/Parameters/FileHash.cs
@@ -0,0 +1,11 @@
+﻿using vtFrontend.lib.Parameters.Base;
+
+namespace vtFrontend.lib.Parameters
+{
+    public class FileHash : BaseParameter
+    {
+        public override string Label => "File Hash (SHA1/SHA256)";
+        
+        public override bool IsRequired => true;
+    }
+}
