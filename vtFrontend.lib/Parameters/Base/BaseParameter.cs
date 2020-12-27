3eef72d613ecb263b8a52fc7990bb134e63bdc92
Jarred Capellman <jcapellman@hotmail.com>
2020-12-27 15:08:06 -0600

    Added: Initial work on the BaseParameter class

---
+++ vtFrontend.lib/Parameters/Base/BaseParameter.cs
@@ -0,0 +1,11 @@
+﻿namespace vtFrontend.lib.Parameters.Base
+{
+    public abstract class BaseParameter
+    {
+        public abstract string Label { get; }
+        
+        public abstract bool IsRequired { get; }
+        
+        public string Value { get; set; }
+    }
+}
