cde065c69dfbef1fa91cb744f1a94af0a3b8c067
Jarred Capellman <jcapellman@hotmail.com>
2020-12-27 15:19:30 -0600

    Change: Swapped the hard coded [0] parameter in the FileDownload

--- vtFrontend.lib/APIs/FileDownload.cs
+++ vtFrontend.lib/APIs/FileDownload.cs
@@ -2,6 +2,7 @@
 
 using vtFrontend.lib.APIs.Base;
 using vtFrontend.lib.Objects;
+using vtFrontend.lib.Parameters;
 using vtFrontend.lib.Parameters.Base;
 
 namespace vtFrontend.lib.APIs
@@ -11,10 +12,14 @@ namespace vtFrontend.lib.APIs
         public FileDownload(SettingsItem settings) : base(settings)
         {
         }
-        
+
+        public override string[] Parameters => new[] {nameof(FileHash)};
+
         public override async Task<bool> RunAsync(BaseParameter[] parameters)
         {
-            var result = await GetByteAsync($"files/{parameters[0].Value}/download");
+            var hash = GetParameterValue(nameof(FileHash), parameters);
+            
+            var result = await GetByteAsync($"files/{hash}/download");
 
             return result != null && result.Length > 0;
         }
