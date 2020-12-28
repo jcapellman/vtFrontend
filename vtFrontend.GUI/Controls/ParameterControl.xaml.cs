68958bfccb274d8fec0c6201bcd19cf0eba9cda9
Jarred Capellman <jcapellman@hotmail.com>
2020-12-28 14:35:31 -0600

    Added: Initial commit of the ParameterControl class

---
+++ vtFrontend.GUI/Controls/ParameterControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+using vtFrontend.GUI.Controls.ViewModels;
+using vtFrontend.lib.Parameters.Base;
+
+namespace vtFrontend.GUI.Controls
+{
+    public partial class ParameterControl : UserControl
+    {
+        
+        
+        public ParameterControl()
+        {
+            InitializeComponent();
+
+            DataContext = new ParameterControlViewModel();
+        }
+
+        public event EventHandler<BaseParameter> OnParameterValueChanged;
+        
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnParameterValueChanged?.Invoke(sender, ((ParameterControlViewModel)DataContext).Parameter);    
+        }
+    }
+}
