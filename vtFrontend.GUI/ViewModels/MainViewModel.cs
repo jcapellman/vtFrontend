72ea2a8da42e0c48590305b9c3db988e9b6a6124
Jarred Capellman <jcapellman@hotmail.com>
2020-12-28 08:33:30 -0600

    Change: Started implementing the MVVM properties

--- vtFrontend.GUI/ViewModels/MainViewModel.cs
+++ vtFrontend.GUI/ViewModels/MainViewModel.cs
@@ -1,21 +1,58 @@
-﻿using vtFrontend.GUI.ViewModels.Base;
+﻿using System.Collections.Generic;
+
+using vtFrontend.GUI.ViewModels.Base;
+using vtFrontend.lib.APIs.Base;
+using vtFrontend.lib.Objects;
 
 namespace vtFrontend.GUI.ViewModels
 {
     public class MainViewModel : BaseVewModel
     {
-        private string _vtKey;
+        private SettingsItem _settings;
 
-        public string VTKey
+        public SettingsItem Settings
         {
-            get => _vtKey;
+            get => _settings;
 
             set
             {
-                _vtKey = value;
+                _settings = value;
 
                 OnPropertyChanged();
             }
+        }
+
+        private List<BaseAPI> _apis;
+
+        public List<BaseAPI> Apis
+        {
+            get => _apis;
+
+            set
+            {
+                _apis = value;
+                
+                OnPropertyChanged();
+            }
+        }
+
+        private BaseAPI _selectedApi;
+
+        public BaseAPI SelectedApi
+        {
+            get => _selectedApi;
+
+            set
+            {
+                _selectedApi = value;
+                
+                OnPropertyChanged();
+            }
+        }
+        
+        public MainViewModel()
+        {
+            Apis = BaseAPI.GetApis();
         }
     }
 }
