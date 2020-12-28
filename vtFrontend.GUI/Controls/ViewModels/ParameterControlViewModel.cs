54ffd8b1cd32351b319c0c14266efc6c82866a01
Jarred Capellman <jcapellman@hotmail.com>
2020-12-28 14:36:50 -0600

    Added: Initial commit of the ParameterControlViewModel class

---
+++ vtFrontend.GUI/Controls/ViewModels/ParameterControlViewModel.cs
@@ -0,0 +1,22 @@
+﻿using vtFrontend.GUI.ViewModels.Base;
+using vtFrontend.lib.Parameters.Base;
+
+namespace vtFrontend.GUI.Controls.ViewModels
+{
+    public class ParameterControlViewModel : BaseVewModel
+    {
+        private BaseParameter _parameter;
+
+        public BaseParameter Parameter
+        {
+            get => _parameter;
+
+            set
+            {
+                _parameter = value;
+                
+                OnPropertyChanged();
+            }
+        }
+    }
+}
